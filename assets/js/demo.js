40cc036c0289fdb9a990d1eb06b6e0fd6f5992ea
dulldesk <54601393+dulldesk@users.noreply.github.com>
2021-12-29 14:25:55 -0500

    Update damper
    Co-authored-by: hewmatt10 <56776292+hewmatt10@users.noreply.github.com>

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -20,6 +20,7 @@ const getMass = () => getInput("m");
 const getK = () => getInput("k");
 const getX0 = () => getInput("x0");
 const getB = () => getInput("b");
+const e = Math.E;
 
 drawWall();
 drawAxis();
@@ -48,10 +49,43 @@ function animate(t) {
 	req = requestAnimationFrame(animate);
 }
 function calculateXNoDamper(t, init_dist=DIST_FROM_WALL) {
-	return getX0() * Math.cos(t/40 * Math.sqrt(getK() / getMass()))+init_dist;
+	let ret = getX0() * Math.cos(t/40 * Math.sqrt(getK() / getMass()));
+	console.log(ret);
+	return ret + init_dist;
 }
 function calculateXWithDamper(t, init_dist=DIST_FROM_WALL) {
-	return 200;
+  	let x0 = getX0();
+	let m = getMass();
+    let k = getK();
+    let b = getB();
+	let d = b * b - 4 * m * k;
+	let ret = 0;
+	if (Math.abs(d - 0) < 0.00001){
+		// critically damped
+		console.log("crtically damped");
+		let r = -1 * b / (2 * m);
+		let c1 = x0;
+		let c2 = -1 * r * x0;
+		ret = c1 * Math.pow(e, r * t) + c2 * t * Math.pow(e, r * t);
+	}
+	else if (d > 0){
+		// overdamped
+		console.log("overdamped");
+		let r1 = (-1 * b + Math.sqrt(d))/(2 * m);
+		let r2 = (-1 * b - Math.sqrt(d))/(2 * m);
+		let c1 = (x0 * r2)/(r2 - r1);
+		let c2 = (x0 * r1)/(r1 - r2);
+		ret = c1 * Math.pow(e, r1 * t) + c2 * Math.pow(e, r2 * t);
+	}
+	else{
+		// underdamped
+		console.log("underdamped");
+		let λ = (-1 * b)/(2 * m)
+		let μ = (Math.sqrt(-1 * d))/(2 * m);
+		ret = x0 * Math.pow(e, λ * t) * Math.cos(μ * t); 
+	}
+	console.log(ret);
+	return ret + init_dist;
 }
 function drawSpring(mass_x, k) {
 	ctx.fillStyle = 'black';
@@ -75,7 +109,7 @@ function drawAxis() {
 	ctx.font = '20px serif';
 
 	// aligned with left, not centre, of mass...
-	ctx.fillText('0', DIST_FROM_WALL-5, CV.H-25);
+	// ctx.fillText('0', DIST_FROM_WALL-5, CV.H-25);
 }
 function drawWall(w=WALL.W, h=WALL.H) {
 	ctx.fillStyle = '#7d5938';
@@ -100,16 +134,23 @@ function getInput(id) {
 }
 
 function onInputChange() {
+	cancelAnimationFrame(req);
 	setOutputs(getK(), getMass());
+	req = requestAnimationFrame(animate);
+	previousTimeStamp =-1;
 }
 function toggleDamper() {
-	// cancelAnimationFrame(req);
+    cancelAnimationFrame(req);
 
-	if ($("#damper-input").is(":visible")) {
-		$("#damper-input").hide();
-		calculateX = calculateXNoDamper;
-	} else {
-		$("#damper-input").show();
-		calculateX = calculateXWithDamper;
-	}
+    if ($("#damper-input").is(":visible")) {
+        $("#damper-input").hide();
+		$(".outputs").show()
+        calculateX = calculateXNoDamper;
+    } else {
+        $("#damper-input").show();
+		$(".outputs").hide()
+        calculateX = calculateXWithDamper;
+    }
+	req = requestAnimationFrame(animate);
+	previousTimeStamp =-1;
 }
