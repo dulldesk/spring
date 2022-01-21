4fe04a9f3125fb6dbcf26051beff60e5ae1fc8c6
dulldesk <54601393+dulldesk@users.noreply.github.com>
2022-01-20 19:55:30 -0500

    Move frills to a new file

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -232,27 +232,3 @@ function resetTime() {
 	ts = 0;
 	previousTimeStamp = -1;
 }
-
-$("#demo-wrapper").click(evt => {
-	evt.stopPropagation();
-	if (evt.target.id === "demo-wrapper" && (evt.clientY - evt.target.getBoundingClientRect().top <= 28)) {
-		toggleDemo();
-	}
-});
-
-function toggleDemo(down) {
-	const demo = $(".demo");
-	if (down || demo.is(":visible")) {
-		cancel();
-		demo.slideUp();
-		$("#demo-wrapper").attr("title", "Click me to maximize the demo again");
-	} else {
-		demo.slideDown();
-		restart(false);
-		$("#demo-wrapper").attr("title", "");
-	}
-}
-
-if (window.location.search.substr(1).split("&").includes("min=1")) {
-    toggleDemo(true);
-}
